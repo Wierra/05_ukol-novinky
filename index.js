1e3932345878a1ec51cf9e320ba4dbc7dacb4aff
Věra Juračáková <juracakova.vera@gmail.com>
2024-10-21 20:51:17 +0200

    Meziuložení 02

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const bodyElement = document.querySelector("body")
 bodyElement.style.backgroundColor = "#e9e9e9"
 
-const info = document.querySelector("news")
+const info = document.querySelector(".news")
 info.style.backgroundColor = "white"
 info.style.width = "60rem"
 
 const mainTitle = document.querySelector("h1")
-mainTitle.classList.add("news__title")
 mainTitle.textContent = "Aktuální novinky"
+mainTitle.classList.add("news__title")
 
-document.querySelector("#zprava1").classList.add = "post--main"
+document.querySelector("#zprava1").classList.add = ".post--main"
 
 document.querySelector("#zprava3").src = "images/zprava3-novy.jpg"
