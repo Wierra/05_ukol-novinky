36c37de087caf515101f8b8dd2ab94126623fa1f
Věra Juračáková <juracakova.vera@gmail.com>
2024-10-21 21:16:25 +0200

    Správné řešení Maruš

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
+/*1. Pomocí document.querySelector vyberte element body a uložte si jej do proměnné bodyElement. Pomocí této proměnné nastavte barvu pozadí elementu na hodnotu #e9e9e9.*/
 const bodyElement = document.querySelector("body")
 bodyElement.style.backgroundColor = "#e9e9e9"
 
-const info = document.querySelector(".news")
-info.style.backgroundColor = "white"
-info.style.width = "60rem"
+/*2. Do jiné proměnné vyberte element s třídou news a nastavte mu barvu pozadí na bílou a maximální šířku na 60rem.*/
+const news = document.querySelector(".news")
+news.style.backgroundColor = "white"
+news.style.maxWidth = "60rem"
 
+/*3. Vyberte element h1 a nastavte mu (v JavaScriptu) třídu na news__title. Nadpis by měl změnit styl. Nastavte také obsah nadpisu na text Aktuální novinky.*/
 const mainTitle = document.querySelector("h1")
-mainTitle.textContent = "Aktuální novinky"
 mainTitle.classList.add("news__title")
+mainTitle.textContent = "Aktuální novinky"
 
-document.querySelector("#zprava1").classList.add = ".post--main"
+/*4. Pomocí atributu id vyberte element obsahující první zprávu. Přidejte do jeho atributu class třídu post--main. První zpráva by tak měla mírně změnit svůj styl.*/
+const news1 = document.querySelector("#zprava1")
+news1.classList.add("post--main")
 
-document.querySelector("#zprava3").src = "images/zprava3-novy.jpg"
+/*5. Pomocí CSS selektoru vyberte obrázek ve třetí zprávě (id zprávy je zprava3) a změňte jeho atribut src na obrázek images/zprava3-novy.jpg*/
+const img3 = document.querySelector("#zprava3 img")
+img3.src = "images/zprava3-novy.jpg"
